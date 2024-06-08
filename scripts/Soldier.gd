15c733c6a91990adff88fc06087f1f5f38d3c687
MasterMurky <64403910+MasterMurky@users.noreply.github.com>
2024-06-09 00:19:40 +0200

    attack animation

--- scripts/Soldier.gd
+++ scripts/Soldier.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
-
+# Movement speed of the character
 var speed = 100
 
-# Target character
+var attack_range = 90
+
+# Reference to the target character
 var target: CharacterBody2D = null
 
-
 @onready var animated_sprite = $AnimatedSprite2D
+
+# Variable to check if the character is attacking
+var is_attacking = false
 
 # Function called every frame
 func _process(delta):
@@ -16,12 +20,20 @@ func _process(delta):
 	if target != null:
 		# Calculate the direction towards the target
 		var direction = (target.global_position - global_position).normalized()
-		# Move the character towards the target
-		velocity = direction * speed
-		move_and_slide()
+		var distance = global_position.distance_to(target.global_position)
+		if distance > attack_range:
+			# Move the character towards the target if not in attack range
+			velocity = direction * speed
+			animated_sprite.play("walk")
+			move_and_slide()
+			is_attacking = false
+		else:
+			# Attack the target if in range
+			if not is_attacking:
+				attack_target()
+			velocity = Vector2.ZERO
 
-		# Update animation and flip
-		animated_sprite.play("walk")
+		# Update sprite flip based on movement direction
 		if direction.x > 0:
 			animated_sprite.flip_h = false
 		elif direction.x < 0:
@@ -30,6 +42,7 @@ func _process(delta):
 		# If no target is defined, the character remains stationary
 		velocity = Vector2.ZERO
 		animated_sprite.play("idle")
+		is_attacking = false
 
 # Function to find the nearest character
 func find_nearest_target():
@@ -47,9 +60,21 @@ func find_nearest_target():
 func update_target():
 	target = find_nearest_target()
 
+# Attack the target
+func attack_target():
+	if target != null:
+		animated_sprite.play("attack1")
+		is_attacking = true
+
+# Function to make the hitbox appear
+# ...
+
+
+
 # Function called when the script is added to the scene
 func _ready():
 	# Add this character to the "characters" group
 	add_to_group("characters")
 	# Ensure the idle animation is playing initially
 	animated_sprite.play("idle")
+	# Connect the animation_finished signal to the _on_AnimatedSprite2D_animation_finished function
